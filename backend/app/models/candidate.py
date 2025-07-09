d3e4af455484918582d392430aac49795ebbc950
vors <jsgd1254@gmail.com>
2025-07-10 00:54:46 +0900

    모델에 예시와 설명 추가

--- backend/app/models/candidate.py
+++ backend/app/models/candidate.py
@@ -2,24 +2,24 @@ from typing import List, Optional
 from pydantic import BaseModel, Field
 
 class Position(BaseModel):
-    title: str
-    companyName: str
-    description: str
-    startEndDate: dict
-    companyLocation: str
+    title: str = Field(..., example="CTO", description="직책")
+    companyName: str = Field(..., example="비바리퍼블리카", description="회사 이름")
+    description: str = Field(..., example="토스쇼핑 검색 서비스 백엔드 아키텍처 설계 및 개발", description="수행한 업무")
+    startEndDate: dict = Field(..., description="근무 기간")
+    companyLocation: str = Field(..., example="대한민국 서울 강남구",description="회사 대략적인 주소")
 
 
 class Education(BaseModel):
-    schoolName: str
-    degreeName: Optional[str]
-    fieldOfStudy: Optional[str]
-    startEndDate: Optional[str]
-    description: Optional[str]
+    schoolName: str = Field(..., example="연세대학교", description="대학교 이름")
+    degreeName: Optional[str] = Field(..., example="학사", description="학/석/박사 표기")
+    fieldOfStudy: Optional[str] = Field(..., example="컴퓨터 공학", description="공부한 분야")
+    startEndDate: Optional[dict] = Field(..., description="재학 기간")
+    description: Optional[str] = Field(..., description="기타 설명")
 
 
 class Candidate(BaseModel):
-    firstName: str
-    lastName: str
+    firstName: str = Field(..., example="Junho", description="이름")
+    lastName: str = Field(..., example="Kim", description="성")
 
     headline: Optional[str] = Field(None, description="LInkedIn 헤드라인")
     summary: Optional[str] = Field(None, description="자기소개/요약 문구")
--- backend/app/models/response.py
+++ backend/app/models/response.py
@@ -1,9 +1,9 @@
 from typing import List
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class ExperienceTag(BaseModel):
-    tag: str
-    evidence: str
+    tag: str = Field(..., example="리더쉽", description="데이터 기반으로 추출한 경험 태그")
+    evidence: str = Field(..., example="CTO, Tech Lead 등 직책 기반 추론", description="데이터 기반으로 추출한 경험 태그의 근거")
 
 
 class InferenceResult(BaseModel):
